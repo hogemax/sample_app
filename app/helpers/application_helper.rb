b10bfe670f298a8b2cbef6711848c258b5d64570
hogemax <jomokoo@gmail.com>
2018-02-28 15:49:47 +0900

    Install the devide

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,19 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+  # Bootstrapによるflashメッセージの装飾に必要な命令 
+  def bootstrap_class_for(flash_type)
+    case flash_type
+      when :success
+        "alert-success"
+      when :error
+        "alert-danger"
+      when :alert
+        "alert-warning"
+      when :notice
+        "alert-info"
+      else
+        flash_type.to_s
+    end
+  end
 end
