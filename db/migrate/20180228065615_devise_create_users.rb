4950bca13bf730b4a05a3843a325eb6dfc0b8203
hogemax <jomokoo@gmail.com>
2018-03-06 00:22:36 +0900

    Finish user destroy action

--- db/migrate/20180228065615_devise_create_users.rb
+++ db/migrate/20180228065615_devise_create_users.rb
@@ -5,6 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :name,              null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.boolean :admin,              default: false
 
       ## Recoverable
       t.string   :reset_password_token
